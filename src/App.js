fd462023fc9a29a41a11be71b8a71a274761fd84
varsil <demo76355@gmail.com>
2023-07-19 21:32:25 +0530

    19-7-23

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import InvoiceForm from './components/InvoiceForm';
 class App extends Component {
   render() {
   return (
-    <div className="App d-flex flex-column align-items-center justify-content-center w-100">
+    <>    <div className="App d-flex flex-column align-items-center justify-content-center w-100">
       <Container>
         <InvoiceForm/>
       </Container>
     </div>
+    </>
+
   );
 }}
 
